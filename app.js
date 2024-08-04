2a56a20e4142ff26118792b0c8e8f862e9efdf04
dmitriyxt5 <dmitriyxdmitriyx@gmail.com>
2024-08-04 18:24:15 +0500

    Операторы сравнения и присваивания

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Привет, мир!")
+let age = 18 + 5;
+age += 2;
+age -=2;
+age *= 2;
+age /= 2;
+age ++
+age --
+console.log(age);
+
+// Операторы сравнения
+const vasya = 20
+console.log(age > vasya);
+console.log(age >= vasya);
+console.log(age < vasya);
+console.log(age <= vasya);
+console.log(age == vasya);
